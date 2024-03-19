c3bb7cd3a668e971a2884973a587627914ff90d7
xlaoru <sia212007@gmail.com>
2024-03-19 17:18:11 +0200

    v2.0

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,11 +1,21 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
-import { selectTodos } from "./store/TodoSlice";
+import { useDispatch, useSelector } from "react-redux";
+
+import { AppDispatch } from "./store";
+
+import { selectTodos, fetchTodos } from "./store/TodoSlice";
 
 import TodoItem from "./TodoItem";
 
 const TodoList = () => {
   const todos = useSelector(selectTodos);
+  const dispatch: AppDispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTodos());
+  }, [dispatch])
+
   return (
     <ul>
       {todos.map((todo: any) => (
--- src/store/TodoSlice.ts
+++ src/store/TodoSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { RootState } from ".";
 
 export interface ITodo {
@@ -7,11 +7,32 @@ export interface ITodo {
   completed: boolean;
 }
 
+type LoadingStatusTypes = 'idle' | 'loading' | 'error'
+
+const setError = (state: any, action: any) => {
+  state.status = "rejected";
+  state.error = action.payload
+}
+
+// Asynchronous thunk action creator
+export const fetchTodos = createAsyncThunk("todos/fetchTodos", async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    if (!response.ok) throw new Error("Failed to fetch todos");
+    return response.json();
+  } catch (error) {
+    console.error("Error fetching todos:", error);
+    throw error;
+  }
+});
+
 export const TodoSlice = createSlice({
   name: "todos",
   initialState: {
     // Here you can create/define your initial state
     todos: [] as ITodo[],
+    status: "idle" as LoadingStatusTypes,
+    error: null,
   },
   reducers: {
     addTodo(state, action) {
@@ -31,6 +52,21 @@ export const TodoSlice = createSlice({
       state.todos = state.todos.filter((todo) => todo.id !== action.payload.id);
     },
   },
+
+  // The "extraReducers" field lets the slice handle actions defined elsewhere
+  extraReducers: (builder) => {
+    builder.addCase(fetchTodos.pending, (state) => {
+      state.status = "loading";
+      state.error = null
+    })
+
+    builder.addCase(fetchTodos.fulfilled, (state, action) => {
+      state.status = "idle";
+      state.todos = action.payload
+    })
+
+    builder.addCase(fetchTodos.rejected, setError)
+  }
 });
 
 export const { addTodo, toggleComplete, removeTodo } = TodoSlice.actions; // Exporting your tools for state management
